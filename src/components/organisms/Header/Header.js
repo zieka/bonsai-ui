1252a486a03ab420a12d10ad93480daaa50f780c
Kyle Scully <scullykns@gmail.com>
2017-10-20 13:10:05 -0700

    added atomic structure

---
+++ src/components/organisms/Header/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from '../../../assets/logo.svg';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Bonsai UI</h1>
+        </header>
+        <p className="App-intro">
+          Large component libraries and UI frameworks tend to offer more than
+          the average consumer uses which can lead to a bunch of overhead and/or
+          extra bundle size for one off uses.
+        </p>
+        <p className="App-intro">
+          Bonsai UI focuses on keeping things simple by starting small.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Header;
