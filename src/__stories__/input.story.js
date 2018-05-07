dafd8acff8323507db171b67783307b3c4aa07bf
Kyle Scully <scullykns@gmail.com>
2018-05-06 22:18:57 -0700

    added input example

---
+++ src/__stories__/input.story.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { checkA11y } from '@storybook/addon-a11y';
+import InputExample from '../components/organisms/input-example/InputExample';
+import centered from '@storybook/addon-centered';
+
+storiesOf('Inputs', module)
+  .addDecorator(centered)
+  .addDecorator(checkA11y)
+  .add('Inputs example', () => <InputExample />);
