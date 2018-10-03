27f321842ca235cf7dbfde2f121e86d93112fb7a
Kyle Scully <scullykns@gmail.com>
2018-10-03 09:30:01 -0700

    ci: travis tweaking

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 eval "$(ssh-agent -s)" #start the ssh agent
-echo $GITHUB_SSH | base64 -D > .travis/deploy_key.pem
-chmod 600 .travis/deploy_key.pem # this key should have push access
-ssh-add .travis/deploy_key.pem
+touch ./deploy_key.pem
+echo $GITHUB_SSH | base64 -D > ./deploy_key.pem
+chmod 600 ./deploy_key.pem # this key should have push access
+ssh-add ./deploy_key.pem
 git remote add deploy git@github.com:zieka/bonsai-ui.git
 git push --follow-tags deploy master
